1f315ad11a5c42e6ad1f890b402378ccdf7fcdfa
edwardbas-pl <edwardbas551@interia.pl>
2020-12-30 13:07:09 +0100

    initial commit

---
+++ run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd $HOME
+
+if [[ ! -f /usr/bin/vim ]]
+then
+	if [[ -f /usr/bin/apt ]]
+	then
+		sudo apt update && sudo apt install vim  wget -y	
+	fi	
+	if [[ -f /usr/bin/pacman ]]
+	then
+		sudo pacman -Sy vim wget --noconfirm
+	fi	
+	if [[ -f /usr/bin/dnf ]]
+	then
+		sudo dnf update && sudo dnf install vim wget -y
+	fi	
+	if [[ -f /usr/bin/dnf ]]
+	then
+		sudo zypper --non-interactive install vim wget
+	fi	
+
+fi
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+wget https://raw.githubusercontent.com/edwardbas-pl/backup/master/vimrc 
+mv vimrc .vimrc
+
+vim +PlugInstall +YcmRestartServer +qall
+
+clear && echo "vim is ready to use"
